9a08bf1256f40702bee49e31d6c50ecbf08605b7
Kirill Nagaitsev <knagaitsev1@gmail.com>
2016-10-28 21:12:58 -0500

    New game files

---
+++ game.js
@@ -0,0 +1,12 @@
+var game = new Phaser.Game(480, 320, Phaser.AUTO, null, {preload: preload, create: create, update: update});
+
+
+function preload() {
+	
+}
+function create() {
+
+}
+function update() {
+
+}
